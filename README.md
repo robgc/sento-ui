dfb92deca9b010f7fc37a54376d01f3b6e377244
Roberto García Calero <roberto.garcia@geographica.com>
2019-04-05 21:43:19 +0200

    Add: Project structure

---
+++ README.md
@@ -0,0 +1,35 @@
+# sento-ui
+
+This project is Sento's Web UI.
+
+_This readme is currently WIP_
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+## License
+
+The source code of this project is licensed under the GNU Affero General
+Public License v3.0.
---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<!--
+Copyright (C) 2019 Roberto García Calero (garcalrob@gmail.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
+<template>
+  <div id="app">
+    <Map></Map>
+  </div>
+</template>
+
+<script>
+import Map from './components/Map.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Map,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
+</style>
